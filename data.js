f0a1f5d89aadb3a2898dbcf9915dbe6f369e2333
YoSheep <134154724+YoSheep@users.noreply.github.com>
2025-05-14 11:31:43 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,49 @@
+window.honorsData = [
+  { year: 2023, title: "第十六届全国大学生信息安全竞赛（创新实践能力赛）华北赛区三等奖" },
+  { year: 2023, title: "天津市大学生信息安全网络攻防大赛一等奖、二等奖" },
+  { year: 2024, title: "第十七届全国大学生信息安全竞赛（创新实践能力赛）华北赛区二等奖" },
+  { year: 2024, title: "第一届“长城杯”信息安全铁人三项半决赛三等奖" },
+  { year: 2024, title: "京津冀大学生信息安全网络攻防大赛一等奖、三等奖" },
+  { year: 2024, title: "2024年强网杯“强网先锋”" },
+  { year: 2024, title: "第四届“网鼎杯”网络安全大赛“网鼎之星”" },
+  { year: 2025, title: "第十八届全国大学生信息安全竞赛（创新实践能力赛）华北赛区一等奖" },
+  { year: 2025, title: "第二届“长城杯”铁人三项赛总决赛三等奖" }
+
+];
+
+window.membersData = [
+  { id: 1, name: "YoSheep", avatar: "img/avatar/yosheep.jpg", blog: "https://yosheep.github.io", description: "根本学不完，根本学不会", year: 2020 },
+  // 2021
+  { id: 2, name: "zegic", avatar: "img/avatar/zegic.jpg", blog: "", description: "虚无的山里灵活的细狗", year: 2021 },
+  { id: 3, name: "La2z0y", avatar: "img/avatar/La2z0y.jpg", blog: "", description: "这个人很懒，什么都没有留下。", year: 2021 },
+  { id: 4, name: "MeowNeko", avatar: "img/avatar/MeowNeko.jpg", blog: "", description: "茶歇区常驻派送员", year: 2021 },
+  { id: 4, name: "Xsheep", avatar: "img/avatar/xsheep.jpg", blog: "https://zyy0808.github.io/", description: "Forever young，Always tearful", year: 2021 },
+  { id: 4, name: "程茉一", avatar: "img/avatar/chengmoyi.jpg", blog: "", description: "下辈子不学计算机", year: 2021 },
+  { id: 4, name: "zeyiwu", avatar: "img/avatar/zeyiwu.jpg", blog: "", description: "在知识的海洋里，希望我是一条大鲨鱼", year: 2021 },
+  // 2022
+  { id: 6, name: "XDLiu", avatar: "img/avatar/xdliu.jpg", blog: "https://liudaxiaxd.github.io/", description: "没问题，我是最强的", year: 2022 },
+  { id: 6, name: "飞鸿", avatar: "img/avatar/feihong.jpg", blog: "", description: "潜伏于比特之间，洞悉万物而无声", year: 2022 },
+  { id: 6, name: "别再熬夜了", avatar: "img/avatar/biezaiaoyele.jpg", blog: "", description: "要不是吃不起苕皮才不会来网络安全", year: 2022 },
+  { id: 6, name: "RZ归零", avatar: "img/avatar/rz.gif", blog: "", description: "Ciallo～(∠・ω< )⌒★", year: 2022 },
+  { id: 6, name: "milimili", avatar: "img/avatar/milimili.jpg", blog: "", description: "性别女，爱好宁", year: 2022 },
+  // 2023
+  { id: 5, name: "Stars messenger", avatar: "img/avatar/starsmessenger.jpg", blog: "", description: "人生缓缓自有答案", year: 2023 },
+  { id: 7, name: "Unicorn", avatar: "img/avatar/unicorn.jpg", blog: "", description: "做个时代先驱者", year: 2023 },
+  { id: 7, name: "Zyyyyyq", avatar: "img/avatar/zyyyyyq.jpg", blog: "", description: "gogogo", year: 2023 },
+  { id: 7, name: "qwqer", avatar: "img/avatar/qwqer.jpg", blog: "", description: "人生漫漫亦灿灿", year: 2023 },
+  { id: 7, name: "地津月老申大爷", avatar: "img/avatar/dijinyuelaoshendaye.jpg", blog: "", description: "怎么我的好友都脱单了", year: 2023 },
+  { id: 5, name: "KUI", avatar: "img/avatar/kui.jpg", blog: "", description: "泪水打湿猪脚饭，发誓要挣一百万", year: 2023 },
+  { id: 7, name: "La0D3ng", avatar: "img/avatar/laodeng.jpg", blog: "", description: "我是钟离的狗", year: 2023 },
+  { id: 7, name: "dimotlo", avatar: "img/avatar/dimotlo.jpg", blog: "", description: "三人行必有我师焉", year: 2023 },
+  { id: 7, name: "0wnerD1ed", avatar: "img/avatar/0wnerD1ed.jpg", blog: "", description: "nubnub", year: 2023 },
+  { id: 7, name: "alexsun", avatar: "img/avatar/alexsun.jpg", blog: "https://alexyun.site", description: "一个不给自己论坛上证书的蠢蛋", year: 2023 },
+  { id: 7, name: "Dawn_ARC", avatar: "img/avatar/Dawn_ARC.jpg", blog: "", description: "我打CTF？真的假的？", year: 2023 },
+  { id: 7, name: "N0rth_5ea", avatar: "img/avatar/InWhere.jpg", blog: "www.bing.com", description: "一片空白下是乱七八糟", year: 2023 },
+  //2024
+  { id: 5, name: "星辰", avatar: "img/avatar/xingchen.jpg", blog: "", description: "唯一的限制是你给自己设定的限制。", year: 2024 },
+  { id: 5, name: "游标卡尺", avatar: "img/avatar/youbiaokachi.jpg", blog: "", description: "积极有趣抽象的唐山人", year: 2024 },
+  { id: 5, name: "YZY", avatar: "img/avatar/yzy.jpg", blog: "", description: "看透生活并依旧热爱生活", year: 2024 },
+  { id: 5, name: "Gh0stF0xy", avatar: "img/avatar/Gh0stF0xy.jpg", blog: "https://gh0stf0xy.github.io", description: "来自信安三班的CTFer一枚～", year: 2024 },
+  { id: 5, name: "oyoung", avatar: "img/avatar/oyoung.jpg", blog: "", description: "flag{there is no flag}", year: 2024 }
+];
+    
